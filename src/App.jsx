31999d451fd308bc9f5dea78b1e3b212fec234ad
FilipFekete <fifek23@student.sdu.dk>
2025-08-18 15:40:02 +0200

    tried basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
+
 import './App.css'
 
 const Card  = ({ title } ) => {
+    const [hasLiked, setHasLiked] = useState(false);
+    const [count, setCount] = useState(0)
+    useEffect(() => {
+        console.log(`${title} has beed liked: ${hasLiked}`);
+    }, [hasLiked]);
+
+    useEffect(() => {
+    console.log('CARD RENDERED');
+    }, []);
     return(
-        <div>
-            <h2>{title}</h2>
+        <div className="card" onClick={() => setCount((prevState) => prevState + 1)}>
+            <h2>{title} <br/> {count || null}</h2>
+
+            <button onClick={()=> setHasLiked(!hasLiked) }>
+                {hasLiked ? 'Liked' : 'Like'}
+            </button>
         </div>
     )
 }
 
 
 const App = () => {
+    const [hideAllMovies, setHideAllMovies] = useState(false);
+
+
     return (
-        <div>
+        <div className="card-container">
             <h2>Functional Arrow Component</h2>
             <Card title="Star Wars" rating={5} isCool={true} actors={[{name: 'Actors'}]}/>
             <Card title="Avatar"/>
